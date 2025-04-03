c37ce00d6437be83203fbfee5e77ba919adfbe85
ganeshprasathagp <ganeshprasathagp@gmail.com>
2025-04-03 21:03:43 +0530

    retreive

--- main.js
+++ main.js
@@ -502,23 +502,7 @@ filterButtons.forEach(button => {
         });
       }
     }
-    
-
-    document.addEventListener('DOMContentLoaded', function() {
-      const menuToggle = document.querySelector('.menu-toggle');
-      const navMenu = document.querySelector('.nav-menu');
   
-      if (menuToggle) {
-          menuToggle.addEventListener('click', function() {
-              this.classList.toggle('active');
-              navMenu.classList.toggle('active');
-              document.body.classList.toggle('no-scroll');
-          });
-      }
-  });
-  
-
-
 
 
     // Track page view on load
